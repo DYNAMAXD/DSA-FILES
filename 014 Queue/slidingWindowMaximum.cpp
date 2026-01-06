a046f3308d1534b0c2d958126f7140a4a326b463
DYNAMAXD <dynamicdivyansh925@gmail.com>
2026-01-06 10:04:58 +0530

    slidingWIndows

---
+++ 014 Queue/slidingWindowMaximum.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void printMaxInWindow(vector<int>& nums , int k)
+{
+    
+}
+
+int main(){
+    vector<int>nums;
+    int k;
+
+    printMaxInWindow(nums , k);
+    return 0;
+}
